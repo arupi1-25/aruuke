ffc40b05d5a0bffa2d1bcd3b4f831c3fae4580d0
arupi1-25 <aruu7014@gmail.com>
2025-10-22 10:06:11 +0600

    Ayazhan

--- aruuke.py
+++ aruuke.py
@@ -1,14 +1,2 @@
-while True:
-    name = int(input('number:'))
-    print('start')
-    while name >=0:
-        print(name)
-        name-=1
-    print('the end ')
 
-
-def name (yuuu):
-    list = ['gi','hi']
-    for i in yuuu:
-        list.append(i.upper())
-    print(list)
+print('Artifficial Inteligence ')
