d526757a046c2d5b47d748cba51921941165bb6a
arupi1-25 <aruu7014@gmail.com>
2025-10-22 10:01:15 +0600

    Zhanerke

--- aruuke.py
+++ aruuke.py
@@ -5,3 +5,10 @@ while True:
         print(name)
         name-=1
     print('the end ')
+
+
+def name (yuuu):
+    list = ['gi','hi']
+    for i in yuuu:
+        list.append(i.upper())
+    print(list)
